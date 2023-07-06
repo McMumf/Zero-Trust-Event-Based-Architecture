14f7589689232a0b994650e174d4a2ab4ef31057
Carson <mccaffreycarson@gmail.com>
2023-07-06 13:45:09 -0400

    Initial push

---
+++ java/messagepublisher/src/main/java/com/mcmumf/zerotrust/messagepublisher/MessagePublisherApplication.java
@@ -0,0 +1,13 @@
+package com.mcmumf.zerotrust.messagepublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagePublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagePublisherApplication.class, args);
+	}
+
+}
